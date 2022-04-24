597e3c076f93e23e971b588b73de44004215e85a
Lauren Hoock <laurenh8916@protonmail.com>
2022-04-23 23:53:04 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const elementMain = document.querySelector('#main');
+elementMain.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory';
+newHeader.textContent = 'Lauren is the champion';
+document.body.append(newHeader); 
